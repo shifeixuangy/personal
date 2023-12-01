b025933fb506f587944753807c081eb550bcfcf1
shifeixuan <shifeixuangy2006@126.com>
2023-12-01 16:24:57 +0800

    javaBase-init

---
+++ javaBase/src/test/java/com/sfx/javabase/JavaBaseApplicationTests.java
@@ -0,0 +1,20 @@
+package com.sfx.javabase;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(value = SpringJUnit4ClassRunner.class)
+class JavaBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+
+
+    }
+
+}
